bb2c5efe18ada2761c29fbaaa0a058eeec6a69c3
Dimma Developer <95135541+DimmaEphraim@users.noreply.github.com>
2023-06-29 22:05:49 +0700

    Add files via upload

---
+++ python/Tutorial Cheaking Update.py
@@ -0,0 +1,11 @@
+import time
+
+versi=1
+
+print("Cheak Update Baru...")
+time.sleep(3)
+print("Update Di Temukan!")
+time.sleep(2)
+print(versi, ">>", versi+1)
+time.sleep(1)
+print("Berhasil Di Update Ke", versi+1)
